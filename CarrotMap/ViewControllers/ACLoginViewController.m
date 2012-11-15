7dbdb7178deb417244595d0801e2c739145393f0
amaliujia <amaliujia@163.com>
2012-11-15 20:57:49 +0800

    add:超级童话的界面～～～～美工发力势不可挡～～

--- CarrotMap/ViewControllers/ACLoginViewController.m
+++ CarrotMap/ViewControllers/ACLoginViewController.m
@@ -19,6 +19,8 @@
 @synthesize touristBtn;
 @synthesize orImage;
 @synthesize orLabel;
+@synthesize logoView;
+@synthesize boxView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -51,9 +53,9 @@
     self=[super init];
     if (self) {
         //登录按钮
-        self.loginBtn = [[UIButton alloc] initWithFrame:CGRectMake(30, 160, 260, 40)];
-        [self.loginBtn setBackgroundImage:[UIImage imageNamed:@"login.png"] forState:UIControlStateNormal];
-        [self.loginBtn setTitle:@"人人登录" forState:UIControlStateNormal];
+        self.loginBtn = [[UIButton alloc] initWithFrame:CGRectMake(40,140,90,90)];
+        [self.loginBtn setBackgroundImage:[UIImage imageNamed:@"renren.png"] forState:UIControlStateNormal];
+//        [self.loginBtn setTitle:@"人人登录" forState:UIControlStateNormal];
         [self.loginBtn addTarget:self action:@selector(loginBtnPressed) forControlEvents:UIControlEventTouchUpInside];
         
         //or分隔线
@@ -63,10 +65,10 @@
         self.orLabel.text = @"or";
         
         //游客浏览按钮
-        self.touristBtn = [[UIButton alloc] initWithFrame:CGRectMake(30, 240, 260, 40)];
-        [self.touristBtn setBackgroundImage:[UIImage imageNamed:@"liulan.png"] forState:UIControlStateNormal];
-        [self.touristBtn setTitle:@"以游客身份浏览" forState:UIControlStateNormal];
-        [self.touristBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        self.touristBtn = [[UIButton alloc] initWithFrame:CGRectMake(160,140,80, 90)];
+        [self.touristBtn setBackgroundImage:[UIImage imageNamed:@"guest.png"] forState:UIControlStateNormal];
+//        [self.touristBtn setTitle:@"以游客身份浏览" forState:UIControlStateNormal];
+//        [self.touristBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [self.touristBtn addTarget:self action:@selector(TouristBtnPressed) forControlEvents:UIControlEventTouchUpInside];
         
         
@@ -80,16 +82,30 @@
 {
     [super viewDidLoad];
     
+    
+    self.logoView=[[UIImageView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.logoView.image=[UIImage imageNamed:@"logoview.png"];
+    [self.view addSubview:self.logoView];
+    
+    
+    self.boxView=[[UIImageView alloc] initWithFrame:CGRectMake(20, 70, 280, 265)];
+    self.boxView.image=[UIImage imageNamed:@"box.png"];
+    self.boxView.userInteractionEnabled=YES;
+    [self.view addSubview:self.boxView];
+//    
     //关闭键盘背景按钮
-    UIButton *bgBtn = [[UIButton alloc] initWithFrame:self.view.frame];
-    [bgBtn addTarget:self action:@selector(bgPressed) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:bgBtn];
+//    UIButton *bgBtn = [[UIButton alloc] initWithFrame:self.view.frame];
+//    [bgBtn addTarget:self action:@selector(bgPressed) forControlEvents:UIControlEventTouchUpInside];
+//    [self.view addSubview:bgBtn];
+    
+    
+    
     
     //把控件添加到页面上
-    [self.view addSubview:self.loginBtn];
-    [self.view addSubview:self.orImage];
-    [self.view addSubview:self.orLabel];
-    [self.view addSubview:self.touristBtn];
+    [self.boxView addSubview:self.loginBtn];
+//    [self.view addSubview:self.orImage];
+//    [self.view addSubview:self.orLabel];
+    [self.boxView addSubview:self.touristBtn];
 
 }
 
@@ -101,6 +117,7 @@
     self.orImage=nil;
     self.touristBtn=nil;
     self.orLabel=nil;
+    self.logoView=nil;
 
 }
 
